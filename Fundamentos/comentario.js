5e747c73eb31d8510a7713e87eac00d7a8168ea3
Geison <gs_gaara@hotmail.com>
2020-05-20 21:45:03 -0300

    Curso Cod3r Web Moderno

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,16 @@
+// Comentario de uma linha
+console.log('linha 1')
+
+/*
+comentário de 
+multiplas
+linhas
+*/
+console.log('linha 2')
+
+/*
+ * Comentário de
+ * multiplas
+ * linhas
+ */
+console.log('linha 3')
